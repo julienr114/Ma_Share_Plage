da7af11cd9109fd31b6268b03fe3aaa4f3217f85
Rabin Julien <julienr114@gmail.com>
2015-10-15 16:44:55 +0200

    json des plages

--- Mashareplage/public/js/controllers/infoCtrl.js
+++ Mashareplage/public/js/controllers/infoCtrl.js
@@ -1,5 +1,20 @@
 // INFO CONTROLLER
-function infoCtrl($scope, $routeParams) {
+function infoCtrl($scope, $routeParams, infoService) {
     var selectedPlage = $routeParams.plage;
     
+    var ville = {
+        Berck: { title: "Berck sur mer plage" },
+        SaintMalo: { title: "Grande Plage Saint-Malo" },
+        Royan: { title: "Grande Conche Royan" },
+        Hendaye: { title: "Plage des deux Jumeaux Hendaye" },
+        Porquerolles: { title: "Plage d'Argent Ils de Porquerolles" },
+        Ajaccio: { title: "Plage Saint-François Ajaccio" },
+        LaLoupe: { title: "Wild Code Schools Plage La Loupe" }
+        }
+    
+    $scope.title = ville[selectedPlage].title;
+    
 }
+        
+    
+    
