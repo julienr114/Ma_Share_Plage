f60f78d774663828a0a4607215f5d7a893fdcaa4
Makss24 <maximefreland@gmail.com>
2015-10-15 19:04:24 +0200

    ajout du scope_image sur info.html

--- Mashareplage/public/js/controllers/infoCtrl.js
+++ Mashareplage/public/js/controllers/infoCtrl.js
@@ -3,16 +3,17 @@ function infoCtrl($scope, $routeParams, infoService) {
     var selectedPlage = $routeParams.plage;
     
     var ville = {
-        Berck: { title: "Berck sur mer plage", shomid: "berck" },
-        SaintMalo: { title: "Grande Plage Saint-Malo", shomid: "" },
-        Royan: { title: "Grande Conche Royan", shomid: "" },
-        Hendaye: { title: "Plage des deux Jumeaux Hendaye", shomid: "" },
-        Porquerolles: { title: "Plage d'Argent Ils de Porquerolles", shomid: "" },
-        Ajaccio: { title: "Plage Saint-François Ajaccio", shomid: "" },
-        LaLoupe: { title: "Wild Code Schools Plage La Loupe", shomid: "" }
+        Berck: { title: "Berck sur mer plage", shomid: "berck", image: "img/berk_sur_mer_plage.jpg" },
+        SaintMalo: { title: "Grande Plage Saint-Malo", shomid: "", image: "img/st_malo_grande_plage.jpg" },
+        Royan: { title: "Grande Conche Royan", shomid: "", image: "img/royan_grande_conche.jpg" },
+        Hendaye: { title: "Plage des deux Jumeaux Hendaye", shomid: "", image: "img/hendaye_2_jumeaux.jpg" },
+        Porquerolles: { title: "Plage d'Argent Ils de Porquerolles", shomid: "", image: "img/porquerolles_plage_dargent.jpg" },
+        Ajaccio: { title: "Plage Saint-François Ajaccio", shomid: "", image: "img/ajacio_plage_st_françois.jpg" },
+        LaLoupe: { title: "Wild Code Schools Plage La Loupe", shomid: "", image: "img/wcs_plage.jpg" }
         }
     
     $scope.title = ville[selectedPlage].title;
+    $scope.image = ville[selectedPlage].image;
     
     $scope.shom = function(){
         infoService.getshom(ville[selectedPlage].shomid), function(res){
