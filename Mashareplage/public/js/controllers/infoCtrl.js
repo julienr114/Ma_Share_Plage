54c1b04f0f1c3d5ebec57e8d8c6fd722e44bcbc9
Makss24 <maximefreland@gmail.com>
2015-10-16 04:45:37 +0200

    Ajout api google + modif structure col-md/api

--- Mashareplage/public/js/controllers/infoCtrl.js
+++ Mashareplage/public/js/controllers/infoCtrl.js
@@ -3,18 +3,18 @@ function infoCtrl($scope, $routeParams, infoService) {
     var selectedPlage = $routeParams.plage;
     
     var ville = {
-        Berck: { title: "Berck sur mer plage", shomid: "berck", weatherid: "3033415", image: "img/berk_sur_mer_plage.jpg", lat:"", lng: "" },
-        SaintMalo: { title: "Grande Plage Saint-Malo", shomid: "saint-malo", weatherid: "2978640", image: "img/st_malo_grande_plage.jpg", lat: "", lng: "" },
-        Royan: { title: "Grande Conche Royan", shomid: "royan", weatherid: "2982343", image: "img/royan_grande_conche.jpg", lat: "", lng: "" },
-        Hendaye: { title: "Plage des deux Jumeaux Hendaye", shomid: "hendaye", weatherid: "3013534", image: "img/hendaye_2_jumeaux.jpg", lat: "", lng: "" },
-        Porquerolles: { title: "Plage d'Argent Ils de Porquerolles", shomid: "saint tropez", weatherid: "3012937", image: "img/porquerolles_plage_dargent.jpg", lat: "", lng: "" },
-        Ajaccio: { title: "Plage Saint-François Ajaccio", shomid: "ajaccio", weatherid: "6452235", image: "img/ajacio_plage_st_françois.jpg", lat: "", lng: "" },
-        LaLoupe: { title: "Wild Code Schools Plage La Loupe", shomid: "", weatherid: "3008418", image: "img/wcs_plage.jpg", lat: "", lng: "" }
+        Berck: { title: "Berck sur mer plage", shomid: "berck", weatherid: "3033415", image: "img/berk_sur_mer_plage.jpg", lat: 50.4, lng: 1.6 },
+        SaintMalo: { title: "Grande Plage Saint-Malo", shomid: "saint-malo", weatherid: "2978640", image: "img/st_malo_grande_plage.jpg", lat: 48.65, lng: -2.02 },
+        Royan: { title: "Grande Conche Royan", shomid: "royan", weatherid: "2982343", image: "img/royan_grande_conche.jpg", lat: 45.63, lng: -1.03 },
+        Hendaye: { title: "Plage des deux Jumeaux Hendaye", shomid: "hendaye", weatherid: "3013534", image: "img/hendaye_2_jumeaux.jpg", lat: 43.37, lng: -1.77 },
+        Porquerolles: { title: "Plage d'Argent Ils de Porquerolles", shomid: "saint tropez", weatherid: "3012937", image: "img/porquerolles_plage_dargent.jpg", lat: 43.12, lng: 6.14 },
+        Ajaccio: { title: "Plage Saint-François Ajaccio", shomid: "ajaccio", weatherid: "6452235", image: "img/ajacio_plage_st_françois.jpg", lat: 41.92, lng: 8.73 },
+        LaLoupe: { title: "Wild Code Schools Plage La Loupe", shomid: "", weatherid: "3008418", image: "img/wcs_plage.jpg", lat: 48.47, lng: 1.02 }
         }
     
     $scope.title = ville[selectedPlage].title;
     $scope.image = ville[selectedPlage].image;
-    
+
     $scope.shom = function(){
         infoService.getshom(ville[selectedPlage].shomid).then(function(res){
             
@@ -41,9 +41,23 @@ function infoCtrl($scope, $routeParams, infoService) {
     
     $scope.weather();
     
-    
-    
+    var myLatLng = new google.maps.LatLng(ville[selectedPlage].lat, ville[selectedPlage].lng);
+
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById('googleMap'), {
+        center: (myLatLng),
+        scrollwheel: true,
+        zoom: 10
+      });
+
+
+      // Create a marker and set its position.
+      var marker = new google.maps.Marker({
+        map: map,
+        position: (myLatLng),
+        title: $scope.plage
+      });    
     
 }
-        
+
     
